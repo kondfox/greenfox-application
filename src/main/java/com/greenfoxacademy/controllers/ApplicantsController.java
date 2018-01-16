38d58ffe77a8b079d7d2e8fb4c5e32e569dbb82b
kondfox <peter.tamas@greenfoxacademy.com>
2018-01-16 10:53:50 +0100

    feat(Applicant controller): implement GET /  showIndexPage endpoint

--- src/main/java/com/greenfoxacademy/controllers/ApplicantsController.java
+++ src/main/java/com/greenfoxacademy/controllers/ApplicantsController.java
@@ -1,14 +1,24 @@
 package com.greenfoxacademy.controllers;
 
+import com.greenfoxacademy.models.Applicant;
+import com.greenfoxacademy.services.ApplicantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class ApplicantsController {
 
+  @Autowired
+  ApplicantService applicantService;
+
   @GetMapping("")
   public String showIndexPage(Model model) {
-
+    List<Applicant> applicants = applicantService.getAllApplicants();
+    model.addAttribute("applicants", applicants);
+    return "index";
   }
 }

b33db172f237eb03ac7cf3463f8583cf3f2f81fa
kondfox <peter.tamas@greenfoxacademy.com>
2018-01-16 10:27:54 +0100

    init(green fox application): init app

---
+++ src/main/java/com/greenfoxacademy/GreenfoxapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenfoxapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenfoxapplicationApplication.class, args);
+	}
+}

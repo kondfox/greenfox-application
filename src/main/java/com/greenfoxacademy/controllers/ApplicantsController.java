0c161dc750010370bd0c269339a17c52eb0b4c86
kondfox <peter.tamas@greenfoxacademy.com>
2018-01-16 11:47:38 +0100

    feat(Applicant controller): implement POST /delete/{applicantId}

--- src/main/java/com/greenfoxacademy/controllers/ApplicantsController.java
+++ src/main/java/com/greenfoxacademy/controllers/ApplicantsController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.applet.Applet;
@@ -37,5 +38,11 @@ public class ApplicantsController {
   public String addApplicant(@ModelAttribute Applicant applicant) {
     applicantService.addApplicant(applicant);
     return "redirect:";
+  }
+
+  @PostMapping("/delete/{applicantId}")
+  public String deleteApplicant(@PathVariable int applicantId) {
+    applicantService.deleteApplicant(applicantId);
+    return "redirect:/";
   }
 }

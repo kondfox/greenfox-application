89674dc389b9b7d0686db150125dd0602bbb4bcd
kondfox <peter.tamas@greenfoxacademy.com>
2018-01-16 10:45:28 +0100

    feat(Applicant service): create ApplicantService interfcae

---
+++ src/main/java/com/greenfoxacademy/services/ApplicantService.java
@@ -0,0 +1,17 @@
+package com.greenfoxacademy.services;
+
+import com.greenfoxacademy.models.Applicant;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ApplicantService {
+
+  List<Applicant> getAllApplicants();
+  Applicant getApplicantById(int id);
+  void modifyApplicant(Applicant applicant);
+  void deleteApplicant(int id);
+  void addApplicant(Applicant applicant);
+
+}

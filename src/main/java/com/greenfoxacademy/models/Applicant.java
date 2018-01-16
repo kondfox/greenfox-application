2be74f2aed44aba4e844f4f4a1353cd559ac416d
kondfox <peter.tamas@greenfoxacademy.com>
2018-01-16 10:32:37 +0100

    feat(Applicant): create Applicant model with constructors and getters, setters

---
+++ src/main/java/com/greenfoxacademy/models/Applicant.java
@@ -0,0 +1,47 @@
+package com.greenfoxacademy.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Applicant {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+  private String name;
+  private boolean isApplied;
+
+  public Applicant() {
+  }
+
+  public Applicant(String name) {
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean isApplied() {
+    return isApplied;
+  }
+
+  public void setApplied(boolean applied) {
+    isApplied = applied;
+  }
+}
